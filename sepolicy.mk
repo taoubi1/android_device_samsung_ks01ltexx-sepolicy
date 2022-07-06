a58035be90d9617e9a17282cef7c18f85c7bcff5
Default Profile <ptaoubi@gmail.com>
2022-07-06 19:33:02 +0100

    TODO: make clean

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,25 +1,25 @@
 # Board specific SELinux policy variable definitions
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/qcom/sepolicy-legacy/common \
-    device/qcom/sepolicy-legacy/ssg \
-    device/qcom/sepolicy-legacy/$(TARGET_BOARD_PLATFORM)
+    device/samsung/ks01ltexx-sepolicy/common \
+    device/samsung/ks01ltexx-sepolicy/ssg \
+    device/samsung/ks01ltexx-sepolicy/msm8974
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/qcom/sepolicy-legacy/test
+    device/samsung/ks01ltexx-sepolicy/test
 
 SELINUX_IGNORE_NEVERALLOWS := true
 endif
 
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += \
-    device/qcom/sepolicy-legacy/public
+    device/samsung/ks01ltexx-sepolicy/public
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    device/qcom/sepolicy-legacy/private
+    device/samsung/ks01ltexx-sepolicy/private
 
 # Board specific SELinux policy variable definitions for legacy devices
 BOARD_VENDOR_SEPOLICY_DIRS += \
-    device/qcom/sepolicy-legacy/legacy-common
+    device/samsung/ks01ltexx-sepolicy/legacy-common
 
 # Add sepolicy version to support OS upgrade and backward compatibility
 BOARD_SEPOLICY_VERS := $(PLATFORM_SDK_VERSION).0
